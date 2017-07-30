f2e53d8d30e7d1c66d4f6a480b6ac5248924b5a0
Sven Scharfenberg <sscharfenberg@ivugmbh.de>
2017-07-30 15:10:34 +0200

    oh well.

---
+++ server/controllers/auth/resend.js
@@ -0,0 +1,123 @@
+/***********************************************************************************************************************
+ *
+ * authController
+ *
+ **********************************************************************************************************************/
+const chalk = require("chalk"); // https://www.npmjs.com/package/chalk
+const crypto = require("crypto"); // https://nodejs.org/api/crypto.html
+const i18n = require("i18n"); // https://github.com/mashpie/i18n-node
+const moment = require("moment"); // https://momentjs.com/
+const mongoose = require("mongoose"); // http://mongoosejs.com/
+const { getCaptcha } = require("../../handlers/captcha");
+const logger = require("../../handlers/logger/console");
+const mail = require("../../handlers/mail");
+const cfg = require("../../config");
+const User = mongoose.model("User");
+
+
+/*
+ * show "resend activation email" form =================================================================================
+ * @param {ExpressHTTPRequest} req
+ * @param {ExpressHTTPResponse} res
+ */
+exports.showResendForm = (req, res) => {
+    const captcha = getCaptcha();
+    req.session.captcha = captcha.text;
+    res.render("auth/resend", {
+        title: i18n.__("APP.RESEND.TITLE"),
+        session: req.session,
+        captcha: captcha.data,
+        flashes: req.flash()
+    });
+};
+
+
+/*
+ * validate resend request =============================================================================================
+ * @param {ExpressHTTPRequest} req
+ * @param {ExpressHTTPResponse} res
+ * @param {callback} next
+ */
+exports.validateResend = async (req, res, next) => {
+    logger.info(
+        `[App] resend activation email request for ${chalk.cyan(
+            req.body.email
+        )}`
+    );
+    const emailUser = await User.findOne({ email: req.body.email });
+    const errors = [];
+    let captchaFail = false;
+    if (!emailUser) {
+        errors.push(i18n.__("APP.RESEND.ERR_EmailNotFound"));
+    }
+    if (
+        emailUser &&
+        emailUser.emailConfirmed &&
+        emailUser.emailConfirmationToken === ""
+    ) {
+        errors.push(i18n.__("APP.RESEND.ERR_EmailAlreadyConfirmed"));
+    }
+
+    if (req.body.captcha === "") {
+        captchaFail = i18n.__("APP.REGISTER.ERROR.CaptchaEmpty");
+    } else if (req.body.captcha !== req.session.captcha) {
+        captchaFail = i18n.__("APP.REGISTER.ERROR.CaptchaMismatch");
+    }
+
+    if (captchaFail) {
+        const captcha = getCaptcha(req.session.captcha);
+        return res.render("auth/resend", {
+            title: i18n.__("APP.RESEND.TITLE"),
+            session: req.session,
+            data: req.body,
+            captcha,
+            captchaFail,
+            flashes: req.flash()
+        });
+    }
+
+    if (errors.length) {
+        const captcha = getCaptcha();
+        req.session.captcha = captcha.text;
+        return res.render("auth/resend", {
+            title: i18n.__("APP.RESEND.TITLE"),
+            session: req.session,
+            data: req.body,
+            captcha: captcha.data,
+            captchaFail,
+            errors: errors,
+            flashes: req.flash()
+        });
+    }
+
+    next();
+};
+
+
+/*
+ * resend activation link ==============================================================================================
+ * @param {ExpressHTTPRequest} req
+ * @param {ExpressHTTPResponse} res
+ */
+exports.doResend = async (req, res) => {
+    const user = await User.findOne({ email: req.body.email });
+    user.emailConfirmationToken = crypto.randomBytes(20).toString("hex");
+    user.emailConfirmationExpires = moment().add(1, "hours");
+    user.emailConfirmed = false;
+    const confirmURL = `http://${req.headers
+        .host}/auth/confirm/${user.emailConfirmationToken}`;
+    await user.save();
+    await mail.send({
+        user,
+        filename: "resend_email",
+        subject: i18n.__("APP.RESEND.MAIL_SUBJECT", cfg.app.title),
+        confirmURL
+    });
+    logger.info(
+        `[App] re-sent activation email to ${chalk.yellow(
+            user.email
+        )} and updated user with new confirm token.`
+    );
+    req.flash("success", i18n.__("APP.RESEND.SUCCESS"));
+    return res.redirect("/");
+};
