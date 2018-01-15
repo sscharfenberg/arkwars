13e5c831d9c0adb6a49311bc5b999c5471535d01
Sven Scharfenberg <sscharfenberg@ivugmbh.de>
2018-01-15 16:45:04 +0100

    fixed a nasty bug where db.seed switched player/npc type chances for planets. no we should not see tomb planets in player homesystems anymore.

--- internals/mockdata/mapData.js
+++ internals/mockdata/mapData.js
@@ -10,6 +10,17 @@ const logger = require("../../server/handlers/logger/console");
 const chalk = require("chalk"); // https://www.npmjs.com/package/chalk
 let planets = [];
 
+let types = {
+    terrestrial: 0,
+    gas: 0,
+    ice: 0,
+    iron: 0,
+    desert: 0,
+    toxic: 0,
+    carbon: 0,
+    tomb: 0
+};
+
 stars.forEach(star => {
     let owner = star.homeSystem ? 2 : 1;
     let numPlanets = seed.getNumPlanets(
@@ -18,11 +29,13 @@ stars.forEach(star => {
         cfg.stars.spectralTypes
     );
     for (let counter = 0; counter < numPlanets; counter++) {
-        planets.push(seed.randomPlanet(star.game, star._id, counter + 1, star.homeSystem));
+        let planet = seed.randomPlanet(star.game, star._id, counter + 1, !star.homeSystem);
+        planets.push(planet);
+        types[planet.type] += 1;
     }
 });
 
-logger.info(`[mockdata] created ${chalk.cyan(planets.length)} planets.`)
+logger.info(`[mockdata] created ${chalk.cyan(planets.length)} planets: ${chalk.yellow(JSON.stringify(types, null, 2))}.`);
 
 module.exports = {
     stars,
