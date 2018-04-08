4dcdf5453a4492c03e7f6bc6b6728cdd7d65deaf
Sven Scharfenberg <sscharfenberg@ivugmbh.de>
2018-04-08 14:29:13 +0200

    refactoring

--- client/game/Shipyards/Design/DesignSection.vue
+++ client/game/Shipyards/Design/DesignSection.vue
@@ -5,18 +5,31 @@
  **********************************************************************************************************************/
 import ScreenSection from "Game/common/ScreenSection/ScreenSection.vue";
 import AvailableShipClasses from "./AvailableShipClasses.vue";
+import Button from "Game/common/Button/Button.vue";
 export default {
     components: {
         ScreenSection,
-        AvailableShipClasses
+        AvailableShipClasses,
+        btn: Button
     },
-    computed: {}
+    computed: {},
+    methods: {
+        newClass () { alert("doh"); }
+    }
 };
 </script>
 
 <template>
     <div>
-        <available-ship-classes />
+        <screen-section class="available">
+            <h3>{{ $t("design.available.title") }}</h3>
+            <btn
+                :on-click="newClass"
+                icon-name="lightbulb"
+                :text-string="$t('design.new.title')"
+                :label="$t('design.new.label')" />
+            <available-ship-classes />
+        </screen-section>
         <screen-section :hdl="$t('design.new.title')">
             <ul>
                 <li>1</li>
@@ -30,6 +43,24 @@ export default {
 
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
+    .available {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        flex-wrap: wrap;
 
+        padding: 1rem;
+
+        background: palette("grey", "sunken");
+
+        > h3 {
+            margin: 0;
+
+            color: palette("brand", "viking");
+
+            font-size: 2.4rem;
+            font-weight: 300;
+        }
+    }
 </style>
